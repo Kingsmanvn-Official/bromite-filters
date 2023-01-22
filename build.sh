64713a81f51ddc86de83534aa6696d219d66c309
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-22 21:28:53 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-xargs < resources -P 4 -L 1 wget -O
+aria2c -i lists 
+perl-rename -i "s/(.*)(\.txt)(\.[0-9]+)$/\1\3\2/" *
+#xargs < resources -P 4 -L 1 wget -O
 echo *.txt | xargs cat > merge-file
 chmod +x ruleset_converter
 ./ruleset_converter --input_format=filter-list \
