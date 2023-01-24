fc1c09649bae580b8fac8f5160b5481e09e79088
luxysiv <46205571+luxysiv@users.noreply.github.com>
2023-01-24 21:15:10 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 xargs < resources -P 4 -L 1 wget -O
-echo *.txt | xargs cat > merge-file
+echo *.txt* | xargs cat > merge-file
 chmod +x ruleset_converter
 ./ruleset_converter --input_format=filter-list \
                     --output_format=unindexed-ruleset \
